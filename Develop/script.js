a957be48652189e03ab50804fdaef07783bc3ac2
Kate McColl <kate@katemccollart.com>
2021-03-25 23:17:47 +1100

    Add: Time Blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,13 @@
 var todayDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(todayDate);
+
+
+$(".saveBtn").click(function () {
+    console.log("Save");
+})
+
+var timeBlock = $(".time-block").clone();
+
+for (let i = 0; i < 7; i++) {
+    timeBlock.clone().appendTo(".container");
+}
